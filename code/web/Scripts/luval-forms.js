35a0c09e893eaf9a2a897081da3b157be4a07b1a
Oscar Marin <oscar.marin@cr.ey.com>
2018-04-21 18:38:15 -0600

    initial commit

---
+++ code/web/Scripts/luval-forms.js
@@ -0,0 +1,80 @@
+﻿class forms {
+    constructor(model) {
+        this.model = model;
+    }
+
+    render(elementId) {
+        var el = document.getElementById(elementId);
+        var form = this.renderForm();
+        el.innerHTML = form;
+    }
+
+    renderForm() {
+        var formBody = this.renderFields();
+        var template = _.template(
+            `<form id="<%= id %>" method:"<%= method %>" action="<%= action %>" >
+                <% formBody %>
+            </form >`
+        );
+        var result = template({ id: this.model.id, method: this.model.method, action: this.model.action, formBody: formBody });
+        return result;
+    }
+
+    renderFields() {
+        var fields = _.sortBy(this.model.fields, 'row');
+        var result = "";
+        for (var i = 0, len = fields.length; i < len; i++) {
+            result = result + this.renderField(this.model.id, fields[i]) + '\n';
+        }
+        return result;
+    }
+
+    renderField(formId, field) {
+        var fieldId = formId + "-" + field.id;
+        var helpElement = null;
+        if (typeof (field.help) != 'undefined' && field.help != null)
+            helpElement = this.renderHelpElement(fieldId, field);
+
+        field["fieldId"] = fieldId;
+        field["input"] = this.renderInput(fieldId, helpElement, field);
+        field["helpElement"] = helpElement;
+
+        var template = _.template(
+            `
+            <div data-row="<%= row %>" class="form-group">
+                <label for="<%= fieldId %>"><%= label %></label>
+                <%= input %>
+                <%= helpElement %>
+            </div>
+            `
+        );
+        var result = template(field);
+    }
+
+    renderInput(fieldId, helpElementTag, field) {
+        if (typeof (field.value) == 'undefined' && field.value == null)
+            field.value = '';
+
+        var template = _.template(
+            `
+            <input id="<%= fieldId %>" name="<%= name %>" type="<%= type %>" value="<%= value %>" <% helpElementTag %> placeholder="<%= placeholder %>">
+            `
+        );
+        field["fieldId"] = fieldId;
+        if (typeof (helpElementTag) != 'undefined' && helpElementTag != null)
+            field["helpElementTag"] = 'aria-describedby="' + fieldId + '-help"';
+        var result = template(field);
+        return result;
+    }
+
+    renderHelpElement(fieldId, field) {
+        var template = _.template(
+            `
+                <small id="<%= fieldId %>-help" data-help-field="<%= id %>" class="form-text text-muted"><%= help %></small>
+            `
+        );
+        var result = template({ fieldId: fieldId, id: field.id, help: field.help });
+        return result;
+    }
+
+}
---
+++ code/web/Scripts/sample-model.js
@@ -0,0 +1,28 @@
+﻿var simpleForm = {
+    id: "client-form",
+    method: "post",
+    action:"/",
+    title: "Client",
+    fields: [
+        {
+            row: 1,
+            id: "code",
+            name: "Code",
+            label: "Code",
+            placeholder: "Enter code",
+            type: "text",
+            help: "The code identifies the client",
+            attributes: [{ "required": "" }],
+        },
+        {
+            row: 2,
+            id: "name",
+            name: "Name",
+            label: "Name",
+            placeholder: "Enter name",
+            type: "text",
+            help: "The name identifies the client",
+            attributes: [{ "required": "" }],
+        },
+    ]
+}
