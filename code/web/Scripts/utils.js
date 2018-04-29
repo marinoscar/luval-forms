fb25507469817a5c59d4eebc9bebfa0c48af0aba
Oscar Marin <oscar.marin@cr.ey.com>
2018-04-29 17:20:15 -0600

    new class to handle utils

---
+++ code/web/Scripts/utils.js
@@ -0,0 +1,9 @@
+﻿class utils {
+    static isNull(item) {
+        return (typeof (item) == 'undefined' || item == null)
+    }
+
+    static isNullOrEmpty(item) {
+        return (utils.isNull(item) || item == '');
+    }
+}
