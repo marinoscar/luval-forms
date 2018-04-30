0b5dbc375a7c8efb8559f2b172316ebc881dbca2
Oscar Marin <oscar.marin@cr.ey.com>
2018-04-29 21:27:30 -0600

    added new utility classes

---
+++ code/business/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Items = new List<Dictionary<string, object>>(25);
+        }
+        public string Name { get; set; }
+        public List<Dictionary<string, object>> Items { get; set; }
+    }
+}
---
+++ code/business/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    public static class ObjectExtensions
+    {
+        public static string ToSql(this object o)
+        {
+            return SqlFormatter.Format(null, o);
+        }
+
+        public static bool IsNullOrDbNull(this object o)
+        {
+            return null == o || Convert.IsDBNull(o);
+        }
+    }
+}
