5115c08ad558bb1770b57fc49aace51bdeb7f545
Oscar Marin <oscar.marin@cr.ey.com>
2018-05-01 08:00:53 -0600

    Added a Sql Server Database class

---
+++ code/data/SqlDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+    public class SqlDatabase : Database
+    {
+        public SqlDatabase() : this(ConfigurationManager.ConnectionStrings[0].ConnectionString)
+        {
+
+        }
+
+        public SqlDatabase(string connectionString) : base(new DbConnectionProvider(new SqlConnection(connectionString)))
+        {
+
+        }
+    }
+}
