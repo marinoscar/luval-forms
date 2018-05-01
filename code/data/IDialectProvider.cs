3f8da287333154766d360a3985dd913f64bc621e
Oscar Marin <oscar.marin@cr.ey.com>
2018-05-01 08:28:24 -0600

    added a new interface to generate sql scripts

---
+++ code/data/IDialectProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace data
+{
+    public interface IDialectProvider
+    {
+        IEnumerable<string> GetDelete(Entity entity);
+        IEnumerable<string> GetInsert(Entity entity);
+        IEnumerable<string> GetUpdate(Entity entity);
+    }
+}
